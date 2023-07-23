a2a72a59be2d98ff62643b5ad35a5312e09e2f9d
Gnatienko <gnatienko@gmail.com>
2023-07-23 11:43:30 -0500

    Basic subtitles with translation tooltip

--- src/Translation.js
+++ src/Translation.js
@@ -1,6 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Translation = ({ text }) => {
-  return <div className="content">{text}!</div>
+  const [translation, setTranslation] = useState(null)
+
+  async function translateWord(word) {
+    const language = "en"
+    const response = await fetch(
+      `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${language}&tl=uk&dt=t&q=${encodeURIComponent(
+        word
+      )}`
+    )
+    const data = await response.json()
+    const translation = data[0][0][0]
+    setTranslation(translation)
+  }
+
+  translateWord(text)
+
+  return <div className="content">{translation}</div>
 }
 export default Translation

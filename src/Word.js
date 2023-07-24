d44b125a62f486367139354d341d78faa3680c47
Gnatienko <gnatienko@gmail.com>
2023-07-24 14:37:08 -0500

    optimize use translation

--- src/Word.js
+++ src/Word.js
@@ -1,15 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import Translation from "./Translation"
 
 import "./Word.css"
 
 const Word = ({ text }) => {
+  const [showTranslation, setShowTranslation] = useState(false)
+
+  const handleMouseEnter = () => {
+    setShowTranslation(true)
+  }
+
+  const handleMouseLeave = () => {
+    setShowTranslation(false)
+  }
+
   return (
-    <div className="tooltip-container">
+    <div
+      className="tooltip-container"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <div className="content">{text}</div>
-      <div className="tooltip">
-        <Translation text={text} />
-      </div>
+      {showTranslation && (
+        <div className="tooltip">
+          <Translation text={text} />
+        </div>
+      )}
     </div>
   )
 }
